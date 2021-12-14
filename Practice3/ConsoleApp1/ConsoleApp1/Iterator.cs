abab83a771503d60ad3afba4b44fc5c16b809a55
zoders <37346560+zoders@users.noreply.github.com>
2021-12-15 00:42:03 +0300

    Add files via upload

---
+++ Practice3/ConsoleApp1/ConsoleApp1/Iterator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+
+    class CarChecker
+    {
+        public void CheckCar(Garage garage)
+        {
+            ICarIterator iterator = garage.CreateNumerator();
+            while (iterator.HasNext())
+            {
+                Car car = iterator.Next();
+                car.GetModel();
+            }
+        }
+    }
+
+    interface ICarIterator
+    {
+        bool HasNext();
+        Car Next();
+    }
+
+    interface ICarNumerable
+    {
+        ICarIterator CreateNumerator();
+        int Count { get; }
+        Car this[int index] { get; }
+    }
+
+    class Garage : ICarNumerable
+    {
+        private Car[] cars;
+        public Garage(Car[] _cars)
+        {
+            cars = _cars;
+            
+        }
+        public int Count
+        {
+            get { return cars.Length; }
+        }
+
+        public Car this[int index]
+        {
+            get { return cars[index]; }
+        }
+        public ICarIterator CreateNumerator()
+        {
+            return new GarageNumerator(this);
+        }
+    }
+
+    class GarageNumerator : ICarIterator
+    {
+        ICarNumerable aggregate;
+        int index = 0;
+        public GarageNumerator(ICarNumerable a)
+        {
+            aggregate = a;
+        }
+        public bool HasNext()
+        {
+            return index < aggregate.Count;
+        }
+
+        public Car Next()
+        {
+            return aggregate[index++];
+        }
+    }
+}
---
+++ Practice3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car auto = new Car(4, "Volvo", new PetrolMove());
+            auto.Move();
+            auto.Movable = new ElectricMove();
+            auto.Move();
+            Car[] cars = new Car[]
+            {
+                new Car(4, "Nissan", new PetrolMove()),
+                new Car(2, "BMW", new ElectricMove()),
+                new Car(4, "Toyota", new PetrolMove()),
+                auto
+            };
+            Garage garage = new Garage(cars);
+            CarChecker carChecker = new CarChecker();
+            carChecker.CheckCar(garage);
+
+        }
+    }
+
+    interface IMovable
+    {
+        void Move();
+    }
+
+    class PetrolMove : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Перемещение на бензине");
+        }
+    }
+
+    class ElectricMove : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Перемещение на электричестве");
+        }
+    }
+    class Car
+    {
+        protected int passengers;
+        protected string model;
+        public void GetModel()
+        {
+            Console.WriteLine(model);
+        }
+        public Car(int num, string model, IMovable mov)
+        {
+            this.passengers = num;
+            this.model = model;
+            Movable = mov;
+        }
+        public IMovable Movable { private get; set; }
+        public void Move()
+        {
+            Movable.Move();
+        }
+    }
+}
